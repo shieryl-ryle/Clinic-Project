ea95ef82508fe9e0f74e0892c22dbf1a6ad21d49
ishiryle <tendillashieryle@gmail.com>
2023-12-12 04:07:18 +0800

    [shi] [Appointment]

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -20,6 +20,13 @@
         <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <div class="outline">
+                        Schedule Medical Appointment
+                        <div>
+                            <button>Click here</button>
+                            <livewire:student.home.show-schedule />
+                        </div>
+                    </div>
                     <livewire:student.home.show-schedule />
                 </div>
             </div>
--- resources/views/livewire/student/home/show-schedule.blade.php
+++ resources/views/livewire/student/home/show-schedule.blade.php
@@ -7,18 +7,5 @@ new class extends Component {
 }; ?>
 
 <div>
-    <div class="flex">
-        <div class="outline">
-            Schedule Medical Appointment
-            <div>
-                <button>Click here</button>
-            </div>
-        </div>
-        <div class="outline">
-            Schedule Dental Appointment
-            <div>
-                <button>Click here</button>
-            </div>
-        </div>
-    </div>
+
 </div>
